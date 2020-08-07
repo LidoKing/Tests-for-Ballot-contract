562d846b4cf4c983217a8b00d119f686a26fd93d
LidoKing <karenchui@Karens-MacBook-Air.local>
2020-08-07 23:51:36 +0800

    Constructor byte32 to string

--- contracts/Ballot.sol
+++ contracts/Ballot.sol
@@ -29,7 +29,7 @@ contract Ballot {
     Proposal[] public proposals;
 
     /// Create a new ballot to choose one of `proposalNames`.
-    constructor(bytes32[] memory proposalNames) public {
+    constructor(string[] memory proposalNames) public {
         chairperson = msg.sender;
         voters[chairperson].weight = 1;
 
