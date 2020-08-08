9693d4b32216dec3da1bd14d8e34ad5f3ccaae20
LidoKing <karenchui@Karens-MacBook-Air.local>
2020-08-08 11:23:50 +0800

    shorten web3 function call

--- migrations/2_ballot.js
+++ migrations/2_ballot.js
@@ -1,5 +1,10 @@
 const Ballot = artifacts.require("Ballot")
+const Web3 = require('web3')
+
+function hex(_string) {
+  return web3.utils.asciiToHex(_string)
+}
 
 module.exports = function (deployer) {
-  deployer.deploy(Ballot, ['0x0'])
+  deployer.deploy(Ballot, [hex('example'), hex('example1'), hex('example2')])
 }
