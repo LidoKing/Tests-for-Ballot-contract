c50ff2662835cf465da880a8c36b56fadd15cf3b
LidoKing <karenchui@Karens-MacBook-Air.local>
2020-08-08 11:24:21 +0800

    constructor test passed

--- test/ballot.js
+++ test/ballot.js
@@ -1,17 +1,24 @@
 const Ballot = artifacts.require("Ballot");
 var expect = require('chai').expect;
+const Web3 = require('web3')
 
 contract("Ballot", (accounts) => {
   let instance;
   let [bob, alice] = accounts;
+
+  function hex(_string) {
+    return web3.utils.asciiToHex(_string)
+  }
+
   beforeEach(async () => {
-    instance = await Ballot.new(['0x0']);
+    instance = await Ballot.new([hex('example'), hex('example1'), hex('example2')]);
   });
 
-  it("should have bob as chairperson and a proposal", async () => {
+  it("should have bob as chairperson and add proposals", async () => {
     const chairperson = await instance.chairperson.call();
-    const proposalName = await instance.getProposals(0);
+    const proposalHex = await instance.getProposals(2);
+    const proposalName = web3.utils.hexToUtf8(proposalHex);
     expect(chairperson).to.equal(bob);
-    expect(proposalName).to.equal('0x0');
+    expect(proposalName).to.equal('example2');
   });
 });
