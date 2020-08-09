63bbcf5f9f41e4dc4cb0068c9d590198a3c90de8
LidoKing <karenchui@Karens-MacBook-Air.local>
2020-08-09 12:15:47 +0800

    test for vote function and getting the winning proposal

--- test/ballot.js
+++ test/ballot.js
@@ -16,7 +16,7 @@ contract("Ballot", (accounts) => {
 
   it("should have bob as chairperson and initialized with proposals", async () => {
     const chairperson = await instance.chairperson.call();
-    const proposalHex = await instance.getProposals(2);
+    const proposalHex = await instance.getProposalName(2);
     const proposalName = web3.utils.hexToUtf8(proposalHex);
     expect(chairperson).to.equal(bob);
     expect(proposalName).to.equal('example2');
@@ -36,5 +36,22 @@ contract("Ballot", (accounts) => {
     const delegate = await instance.delegate(alice, {from:sam});
     const aliceWeight = await instance.getVoterWeight(alice);
     expect(aliceWeight.toNumber()).to.equal(2);
+  });
+
+  it("should allow authorized voters to vote", async () => {
+    const giveRight = await instance.giveRightToVote(alice, {from: bob});
+    const giveRight1 = await instance.giveRightToVote(sam, {from: bob});
+    const vote = await instance.vote(0, {from: alice});
+    const vote1 = await instance.vote(0, {from: sam});
+    const voteCount = await instance.getProposalVoteCount(0);
+    expect(voteCount.toNumber()).to.equal(2);
+  });
+
+  it("should get the name of the winning proposal", async () => {
+    const giveRight = await instance.giveRightToVote(alice, {from: bob});
+    const vote = await instance.vote(1, {from: alice});
+    const result = await instance.winnerName();
+    const name = web3.utils.hexToUtf8(result);
+    expect(name).to.equal('example1');
   });
 });
